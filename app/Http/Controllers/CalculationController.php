fecff90a27d9a1668230d2c907bc0a1d7727aaf0
Suman Sen <mesuman@yahoo.com>
2024-08-30 11:45:16 +0600

    calcuation updated

--- app/Http/Controllers/CalculationController.php
+++ app/Http/Controllers/CalculationController.php
@@ -19,7 +19,7 @@ class CalculationController extends Controller
         $cabinetAttachItem = $request->input('cabinate_type_data.data_product_specification');
         $extractedItems = $this->extractAttachItem($cabinetAttachItem);
         $cabinateSize=$this->cmToInches($request);
-
+        $quantity = $request->input('quantity',1);
         //door calculation
         $cabinetInteriorMaterialPrice = $request->input('cabinet_interior_material_data.value');
         $doorPrice = $request->input('cabinate_door_style_data.value');
@@ -46,6 +46,7 @@ class CalculationController extends Controller
         $number_of_pullout_shelves_type = $request->input('number_of_pullout_shelves_type'); 
         
         $pullout_shelves_price = 0 ;
+        // dd($this->containsWord($number_of_pullout_shelves_type,'Door'));
         if($this->containsWord($number_of_pullout_shelves_type,'Door')){
             $pullout_shelves_price = $doorPrice;
         }else{
@@ -55,9 +56,8 @@ class CalculationController extends Controller
         $pullout_shelve_calc = [
             'pullout_shelve_single_price'=> round($pullout_shelves_single,2),
             'pullout_shelve_qty'=> $number_of_pullout_shelves,
-            'pullout_shelve_total_price'=> round($pullout_shelves_single* $number_of_fixed_shelves,2)
+            'pullout_shelve_total_price'=> round($pullout_shelves_single*$number_of_pullout_shelves,2)
         ];
-
         //fished side
         $finished_side_data = $request->input('finished_side_data.data_product_name');
         $finishes_side_type = $request->input('finishes_side_type');
@@ -74,7 +74,8 @@ class CalculationController extends Controller
         ];
         // dd($shelveCalc);
         $cabinateInSquarInch = (2*($cabinateSize['h']*$cabinateSize['d']))+($cabinateSize['h']*$cabinateSize['w'])+ (2*($cabinateSize['w']*$cabinateSize['d']));
-        $allPrice = round($cabinateInSquarInch*$cabinetInteriorMaterialPrice)+(round($finished_side['price']))+(round($pullout_shelve_calc['pullout_shelve_total_price']))+(round($fixed_shelves_calc['fixed_shelve_total_price']))+($doorCalc['singleDoorPrice']*$doorCalc['totalDoor'])+($drawarCalc['singleDrawerPrice']*$drawarCalc['totalDrawer'])+round($cabinateInSquarInch*$this->manufaturingCost,2);
+        $allPrice = round($cabinateInSquarInch*$cabinetInteriorMaterialPrice)+(round($finished_side['price']))+
+        (round($pullout_shelve_calc['pullout_shelve_total_price']))+(round($fixed_shelves_calc['fixed_shelve_total_price']))+($doorCalc['singleDoorPrice']*$doorCalc['totalDoor'])+($drawarCalc['singleDrawerPrice']*$drawarCalc['totalDrawer'])+round($cabinateInSquarInch*$this->manufaturingCost,2);
         
         return response()->json([
             'cabinet_attach_item' =>$extractedItems,
@@ -87,7 +88,7 @@ class CalculationController extends Controller
             'finished_side'=>$finished_side,    
             'door'=>$doorCalc,
             'drawer'=>$drawarCalc,
-            'totalPrice'=>$allPrice
+            'totalPrice'=>$allPrice*$quantity
         ]);
     }
 
